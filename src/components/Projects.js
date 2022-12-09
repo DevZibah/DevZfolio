b9431d01339cf36364d87e292d93c1c54f551ca2
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-12-09 08:30:29 -1100

    added the open source project i recently worked on

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import Aos from 'aos'
 import 'aos/dist/aos.css'
 import rest from '../assets/restcountries.jpg'
 import ecommerce from '../assets/Ecommerce.jpg'
+import aidryse from '../assets/aidryse.png'
 import rockps from '../assets/RockPaperScissors.jpg'
 import advicegapp from '../assets/AdviceGApp.jpg'
 import bookmark from '../assets/Bookmark.jpg'
@@ -24,6 +25,75 @@ const Projects = () => {
       >
         <h3 className='text-capitalize prj'>projects</h3>
       </div>
+      <section
+        className='sec-one mt-5 mt-md-4'
+        data-aos='fade-up'
+        data-aos-offset='200'
+        data-aos-easing='ease-in-sine'
+        data-aos-duration='600'
+      >
+        <div className='image-one'>
+          <img src={ecommerce} alt='Ecommerce single page' />
+        </div>
+        <div className='mt-5'>
+          <h4 className='project-name'>E-commerce product page</h4>
+          <p className='mt-4 div-p'>
+            This project enables users to view the optimal layout for the site
+            depending on their device's screen size, see hover states for all
+            interactive elements on the page, open a lightbox gallery by
+            clicking on the large product image, switch the large product image
+            by clicking on the small thumbnail images, add items to the cart,
+            view the cart and remove items from it. It was built with ReactJs
+            and Bootstrap.
+          </p>
+          <div className='mt-4 buttn butn p-3'>
+            <a
+              href='https://e-commerceproductapp.netlify.app/'
+              className='text-capitalize linkk'
+            >
+              see this project
+            </a>
+          </div>
+        </div>
+        <div className='imagee'>
+          <img src={ecommerce} alt='Ecommerce single page' />
+        </div>
+      </section>
+      <section
+        className='sec-one mt-5 mt-md-4'
+        data-aos='fade-up'
+        data-aos-offset='200'
+        data-aos-easing='ease-in-sine'
+        data-aos-duration='600'
+      >
+        <div className='image-one'>
+          <img src={aidryse} alt='aidryse' />
+        </div>
+        <div className='mt-5'>
+          <h4 className='project-name'>Aidryse</h4>
+          <p className='mt-4 div-p'>
+            Aidryse is a web-based open source platform for accessing audio
+            information on Gender Based Violence. In collaboration with South
+            Saharan Social Development Organization (SSDO), we focused our work
+            on a section of their many projects - Gender-based Violence.
+            Understanding that GBV has become a prevalent social ill, we decided
+            to create a platform that educates and shares stories of
+            Gender-based violence in audio format.The technologies used in this
+            project are: ReactJs, Bootstrap, Figma, EmailJs, Cloudinary, and MailTo.
+          </p>
+          <div className='mt-4 buttn butn p-3'>
+            <a
+              href='https://aidryse.netlify.app/'
+              className='text-capitalize linkk'
+            >
+              see this project
+            </a>
+          </div>
+        </div>
+        <div className='imagee'>
+          <img src={aidryse} alt='aidryse' />
+        </div>
+      </section>
       <section
         className='sec-one mt-4'
         data-aos='fade-up'
@@ -60,40 +130,7 @@ const Projects = () => {
           <img src={rest} alt='Rest Countries API' />
         </div>
       </section>
-      <section
-        className='sec-one mt-5 mt-md-4'
-        data-aos='fade-up'
-        data-aos-offset='200'
-        data-aos-easing='ease-in-sine'
-        data-aos-duration='600'
-      >
-        <div className='image-one'>
-          <img src={ecommerce} alt='Ecommerce single page' />
-        </div>
-        <div className='mt-5'>
-          <h4 className='project-name'>E-commerce product page</h4>
-          <p className='mt-4 div-p'>
-            This project enables users to view the optimal layout for the site
-            depending on their device's screen size, see hover states for all
-            interactive elements on the page, open a lightbox gallery by
-            clicking on the large product image, switch the large product image
-            by clicking on the small thumbnail images, add items to the cart,
-            view the cart and remove items from it. It was built with ReactJs
-            and Bootstrap.
-          </p>
-          <div className='mt-4 buttn butn p-3'>
-            <a
-              href='https://e-commerceproductapp.netlify.app/'
-              className='text-capitalize linkk'
-            >
-              see this project
-            </a>
-          </div>
-        </div>
-        <div className='imagee'>
-          <img src={ecommerce} alt='Ecommerce single page' />
-        </div>
-      </section>
+
       <section
         className='sec-one mt-5'
         data-aos='fade-up'
@@ -137,9 +174,15 @@ const Projects = () => {
           <img src={bscroommate} alt='a roommate recommendation system' />
         </div>
         <div className='mt-5'>
-          <h4 className='project-name text-capitalize'>a roommate recommendation system</h4>
+          <h4 className='project-name text-capitalize'>
+            a roommate recommendation system
+          </h4>
           <p className='mt-4 div-p'>
-            Users can sign up for this project, search for roommates, see recommended roommates, and view all users who have registered on this application. This app suggests roommates to users based on the information they entered when signing up for the app. ReactJs and Django were used to create this project.
+            Users can sign up for this project, search for roommates, see
+            recommended roommates, and view all users who have registered on
+            this application. This app suggests roommates to users based on the
+            information they entered when signing up for the app. ReactJs and
+            Django were used to create this project.
           </p>
           <div className='mt-4 buttn butn p-3'>
             <a
